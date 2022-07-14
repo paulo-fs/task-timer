daee63a53452d45eb44e41a2149c27c161e0214c
Paulo Souza <jr.paulo87@gmail.com>
2022-07-14 15:20:49 -0300

    feat: global styles and theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import { ThemeProvider } from 'styled-components'
-import { defaultTheme } from "./styles/theme/defaultTheme"
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from './styles/global';
+import { defaultTheme } from './styles/theme/defaultTheme';
 
 function App() {
-  return (
-    <ThemeProvider theme={defaultTheme}>
-      <h1>hello world</h1>
-    </ThemeProvider>
-  )
+	return (
+		<ThemeProvider theme={defaultTheme}>
+			<GlobalStyle />
+			<h1>hello world</h1>
+		</ThemeProvider>
+	);
 }
 
-export default App
+export default App;
