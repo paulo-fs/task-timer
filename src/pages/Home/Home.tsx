174fdf58333464c3654b90773ee98ba0f2df68cd
Paulo Souza <jr.paulo87@gmail.com>
2022-07-18 10:44:55 -0300

    fix: use the function reset() on the correct place

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -39,9 +39,14 @@ export function Home() {
 	const task = watch('task');
 	const isSubmitDisabled = !task;
 
+	function handleCreateNewCycle (data: FormData) {
+		createNewCycle(data);
+		reset();
+	}
+
 	return (
 		<HomeContainer>
-			<form onSubmit={handleSubmit(createNewCycle)}>
+			<form onSubmit={handleSubmit(handleCreateNewCycle)}>
 				<FormProvider {...createNewCycleForm}>
 					<NewCycleForm />
 				</FormProvider>
